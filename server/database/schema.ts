df682089ca72412b6535b4ccab5a3bfa0612cb29
barbapapazes <e.soubiran25@gmail.com>
2024-12-03 22:18:55 +0100

    feat: add user_id

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -2,5 +2,6 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const tasks = sqliteTable('tasks', {
   id: integer('id').primaryKey({ autoIncrement: true }),
+  userId: integer('user_id'),
   title: text('title'),
 })
